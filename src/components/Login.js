c714f42a3fee90cda9b5bb296b8877404c488252
AlineMSant <alinems4120@gmail.com>
2023-03-09 15:30:20 -0300

    requisito 1 - valida campos de email e senha

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,69 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      disabled: true,
+    };
+
+    this.disableBtn = this.disableBtn.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    }, () => this.disableBtn());
+  }
+
+  disableBtn() {
+    const {
+      email,
+      password,
+    } = this.state;
+
+    const validationEmail = /\S+@\S+\.\S+/;
+    const number = 6;
+
+    if (validationEmail.test(email) && password.length >= number) {
+      this.setState({
+        disabled: false,
+      });
+    } else {
+      this.setState({
+        disabled: true,
+      });
+    }
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <div>
         <input
           type="textbox"
           data-testid="email-input"
+          name="email"
+          onChange={ this.handleChange }
         />
         <input
           type="textbox"
           data-testid="password-input"
+          name="password"
+          onChange={ this.handleChange }
         />
-        <button>Entrar</button>
+        <button
+          disabled={ disabled }
+        >
+          Entrar
+
+        </button>
       </div>
     );
   }
