03be93b41859294167ce09f3bb94ebe7fb84af81
AlineMSant <alinems4120@gmail.com>
2023-03-09 17:02:45 -0300

    requisito 1 e 2 - exclusão de componente login passando códigos para pages Login e Wallet

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import Header from './Header';
 
 class WalletForm extends Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+      <div>WalletForm</div>
     );
   }
 }
